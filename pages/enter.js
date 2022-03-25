7a20b95363fd42021de52faaeeb2f67bbbb3e404
Salsa Steve <salsasteve@metacanny.com>
2022-03-25 15:28:05 -0500

    Added username input and debounced input

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,9 @@
 import { auth, googleAuthProvider } from '../lib/firebase';
-import { signInWithPopup } from 'firebase/auth';
-import { useContext } from 'react';
+import { doc, writeBatch, getDoc, getFirestore } from 'firebase/firestore';
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { useContext, useState, useEffect, useCallback } from 'react';
 import { UserContext } from '../lib/context';
+import debounce from 'lodash.debounce';
 
 export default function Enter({}) {
   const { user, username } = useContext(UserContext);
@@ -21,8 +23,9 @@ export default function Enter({}) {
   );
 }
 
+// Sign out button
 function SignOutButton() {
-  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+  return <button onClick={() => signOut(auth)}>Sign Out</button>;
 }
 
 function SignInButton() {
@@ -49,6 +52,65 @@ function UsernameForm() {
   const [loading, setLoading] = useState(false);
 
   const { user, username } = useContext(UserContext);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create refs for both documents
+    const userDoc = doc(getFirestore(), 'users', user.uid);
+    const usernameDoc = doc(getFirestore(), 'usernames', formValue);
+
+    // Commit both docs together as a batch write.
+    const batch = writeBatch(getFirestore());
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  const onChange = (e) => {
+    // Force form value typed in form to match correct format
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    // Only set form value if length is < 3 OR it passes regex
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  //
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue]);
+
+  // Hit the database for username match after each debounced change
+  // useCallback is required for debounce to work
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = doc(getFirestore(), 'usernames', username);
+        const snap = await getDoc(ref);
+        console.log('Firestore read executed!', snap.exists());
+        setIsValid(!snap.exists());
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
 
   return (
     !username && (
